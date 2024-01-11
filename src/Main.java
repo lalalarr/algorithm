ead5acdfaec48e9ff478e53e5d4cdf4f443c3dd2
JH Han <jhhan@JH-MacBook-Air.local>
2024-01-11 23:34:31 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int N = 1000000;
+        int cnt = 0;
+
+        for(int i =0; i<N; i++){
+            System.out.println(cnt++);
+        }
+    }
+}
